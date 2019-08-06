680479734d014dae4a0fac61d3038afcde76b176
johri21 <abhinav.johri@careem.com>
2019-08-06 16:42:32 +0400

    tenserflow backend GPU count

--- examples/mnist_cnn.py
+++ examples/mnist_cnn.py
@@ -70,3 +70,4 @@ model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+print('GPU {}'.format(K.tensorflow_backend._get_available_gpus()))
